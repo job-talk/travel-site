400b87299bab4abad1ffda86d8d200a01cccecff
job_talk <job-talk@users.noreply.github.com>
2017-07-15 18:48:41 -0500

    Added Gulp task for stylesheet (postcss variables & nesting, autoprefixer)

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 var gulp = require('gulp'),
-    watch = require('gulp-watch');
+    watch = require('gulp-watch'),
+    postcss = require('gulp-postcss'),
+    autoprefixer = require('autoprefixer'),
+    cssvars = require('postcss-simple-vars'),
+    nested = require('postcss-nested');
 
 gulp.task('default', function() {
   console.log("Hooray - you created a Gulp task.");
@@ -12,8 +16,11 @@ gulp.task('html', function() {
 
 
 gulp.task('styles', function() {
-  console.log("Imagine something SASS or PostCSS task running here.");
+  return gulp.src('./app/assets/styles/styles.css')
+    .pipe(postcss([cssvars, nested, autoprefixer]))
+    .pipe(gulp.dest('./app/temp/styles/'));
 });
+
 
 gulp.task('watch', function() {
 
